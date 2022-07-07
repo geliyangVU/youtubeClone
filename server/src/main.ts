3b3d93b3a3fa0eb8ecddb1596755866b0d98ff88
Geli Yang <gyang5@villanova.edu>
2022-07-07 14:48:05 -0400

    add shutdown  functions using signals

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,5 +4,21 @@ const PORT = process.env.PORT || 4000
 
 const app = express()
 
-const server = app.listen(PORT,)
+const server = app.listen(PORT, () =>{
+    console.log("Server Listening to:"+ PORT);
+})
 
+const signals=["SIGTERM" , "SIGINT" ]
+
+function gracefulShutdown(signal :string){
+    process.on(signal , async()=>{
+        server.close()
+    })
+}
+
+
+for(let i=0; i< signals.length; i++){
+    gracefulShutdown(signals[i])
+}
+
+//13.15
