6e397e3dccd7eb5d5c6ffa0f9845963c1e581dd4
Geli Yang <gyang5@villanova.edu>
2022-08-03 16:43:55 -0400

    Implemented gracefulShutdown function
    Implemented gracefulShutdown function, tested locally

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,24 +1,26 @@
-import express from "express"
+import express from "express";
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 const app = express()
 
 const server = app.listen(PORT, () =>{
-    console.log("Server Listening to:"+ PORT);
+    console.log(`Server Listening to: http://Localhost: ${PORT}`);
 })
 
-const signals=["SIGTERM" , "SIGINT" ]
+const signals = ["SIGTERM", "SIGINT"];
 
 function gracefulShutdown(signal :string){
     process.on(signal , async()=>{
-        server.close()
+        server.close();
+        console.log("work is done")
+        process.exit(0);
     })
 }
 
 
 for(let i=0; i< signals.length; i++){
-    gracefulShutdown(signals[i])
+    gracefulShutdown(signals[i]);
 }
 
 //13.15
