5b6089c095e2be71678908fd19d0231ce21be9b9
Geli Yang <gyang5@villanova.edu>
2022-09-16 13:43:53 -0400

    Implement get User routes
    Create middle ware folder.

--- server/src/modules/user/user.route.ts
+++ server/src/modules/user/user.route.ts
@@ -2,21 +2,21 @@ import express from "express";
 import { processRequestBody } from "zod-express-middleware";
 import { registerUserHandler } from "./user.controller";
 import { registerUserSchema } from "./user.schema";
-// import requireUser from "../../";
+
+import requireUser from "../../middleware/requireUser";
 
 const router = express.Router();
 
 //prefix: /api/users
+
+router.get("/", requireUser, (req, res) => {
+  return res.send(res.locals.user);
+});
 
 router.post(
   "/",
   processRequestBody(registerUserSchema.body),
   registerUserHandler
 );
-
-// router.get("/", requireUser, (req, res) => {
-//   return res.send(res.locals.user)
-// }
-// );
 
 export default router;
